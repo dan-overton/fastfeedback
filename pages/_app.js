8f001909d13389f78fb265a1c35b9b1e32012586
Dan Overton <846955+dan-overton@users.noreply.github.com>
2023-01-09 16:50:47 +0000

    Add user storage, CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
-import { ProvideAuth } from '../lib/auth'
-import '../styles/globals.css'
+import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { AuthProvider } from '@/lib/auth'
+import theme from '@/styles/theme'
 
 export default function App({ Component, pageProps }) {
-  return <ProvideAuth>
-    <Component {...pageProps} />
-  </ProvideAuth>
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <ChakraProvider theme={theme}>
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ChakraProvider>
+    </>
+  )
 }
